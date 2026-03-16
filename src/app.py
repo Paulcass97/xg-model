fc4ce54d0e5434f4731530d246051a2b18ce78f1
Paulcass97 <paulcass97@hotmail.com>
2026-03-16 12:19:02 +0000

    Add missing os import

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.patches import Arc
